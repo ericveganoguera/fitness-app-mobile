f6a321f98cdf3debc33d3f210e64b00067fea587
ericveganoguera <eveno229@gmail.com>
2023-06-06 17:24:05 +0200

    Add first Update

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { View } from "react-native";
+
+const NavBar = () =>{
+    return (
+        <View>
+            <View></View>
+            <View></View>
+            <View></View>
+        </View>
+        )
+    }
+
+export default NavBar
